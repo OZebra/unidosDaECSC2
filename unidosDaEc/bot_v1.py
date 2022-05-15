d21b3d9cc4758b50b52fed84ae37970a761bf826
HDIAS-BBTV <hdias@c.bbtv.com>
2022-05-15 14:38:19 -0300

    Expandindo a base, criando Marauders, não construindo mais bunkers, construindo 4*n_bases barracks

--- unidosDaEc/bot_v1.py
+++ unidosDaEc/bot_v1.py
@@ -26,13 +26,13 @@ class unidosDaEc(BotAI):
         defendersNumber = 10
         attackersNumber = 20
         targetEnemy: Point2 = self.enemy_structures.random_or(self.enemy_start_locations[0]).position
-        targetDefender: Point2 = self.start_location.position
+        targetDefender: Point2 = self.structures.furthest_to(self.start_location) # self.start_location.position
         if iteration % 20 == 0:
             await self.distribute_workers()
         
         supplyRatio = self.supply_used /(self.supply_used + self.supply_left);
-        print( "Supply: ", self.supply_left)
-        print("self.townhalls", self.townhalls.amount)
+        # print( "Supply: ", self.supply_left)
+        # print("self.townhalls", self.townhalls.amount)
         if (
             #Caso o supply esteja acabando e a gente tenha bases de controle
             #E a gente pode comprar um novo depósito e não tem nenhum depósito sendo construído
@@ -43,11 +43,14 @@ class unidosDaEc(BotAI):
             # If workers were found
             if workers:
                 worker: Unit = workers.furthest_to(workers.center)
-                location: Point2 = await self.find_placement(UnitTypeId.SUPPLYDEPOT, worker.position, placement_step=3)
+                location: Point2 = await self.find_placement(UnitTypeId.SUPPLYDEPOT, worker.position, placement_step=4)
                 # If a placement location was found
                 if location:
                     # Order worker to build exactly on that location
                     worker.build(UnitTypeId.SUPPLYDEPOT, location)
+        
+        if (self.can_afford(UnitTypeId.COMMANDCENTER)) :
+            await self.expand_now() # Caso a gente possa criar outra base de comando, crie
         
         #Constrói barracks(quartel general)
         #Seria bom a gente entender um pouco melhor as requisições das validações que ele faz pra garantir
@@ -55,8 +58,8 @@ class unidosDaEc(BotAI):
         barracks_tech_requirement: float = self.tech_requirement_progress(UnitTypeId.BARRACKS)
         if (
             barracks_tech_requirement == 1 and
-            self.structures(UnitTypeId.BARRACKS).ready.amount + self.already_pending(UnitTypeId.BARRACKS) < 4 and
-            self.can_afford(UnitTypeId.BARRACKS)
+            self.structures(UnitTypeId.BARRACKS).ready.amount + self.already_pending(UnitTypeId.BARRACKS) < (4 * len(self.structures(UnitTypeId.COMMANDCENTER))) and
+            self.can_afford(UnitTypeId.BARRACKS) and len(self.structures(UnitTypeId.COMMANDCENTER)) > 1 # Espere até ter pelo menos 2 bases de comando, então começe a criar 4 barracks por base de comando
         ):
             workers: Units = self.workers.gathering
             if (
@@ -99,80 +102,94 @@ class unidosDaEc(BotAI):
         # Se temos barracks
         if self.structures(UnitTypeId.BARRACKS).ready.exists:
             # Esses barracks estão construidos
+            cnt = 0
             for lab in self.structures(UnitTypeId.BARRACKS).ready:
                 # Me veja quais habilidades ele tem disponível
                 abilities = await self.get_available_abilities(lab)
                 # Se eu puder fazer um reator e tiver recursos
-                if AbilityId.BUILD_REACTOR_BARRACKS in abilities and \
-                self.can_afford(AbilityId.BUILD_REACTOR_BARRACKS):
-                    # Faça um reator
-                    self.do(lab(AbilityId.BUILD_REACTOR_BARRACKS))
+                if cnt % 4 == 0:
+                    if AbilityId.BUILD_TECHLAB_BARRACKS in abilities and \
+                    self.can_afford(AbilityId.BUILD_TECHLAB_BARRACKS):
+                        self.do(lab(AbilityId.BUILD_TECHLAB_BARRACKS))
+                else:
+                    if AbilityId.BUILD_REACTOR_BARRACKS in abilities and \
+                    self.can_afford(AbilityId.BUILD_REACTOR_BARRACKS):
+                        # Faça um reator
+                        self.do(lab(AbilityId.BUILD_REACTOR_BARRACKS))
+                cnt += 1
                     
-        if len(self.structures(UnitTypeId.BUNKER).ready) < 2:
-            if self.can_afford(UnitTypeId.BUNKER):
-                workers: Units = self.workers.gathering
-                if (
-                    workers and self.townhalls
-                ):
-                    worker: Unit = workers.furthest_to(workers.center)
+        # if len(self.structures(UnitTypeId.BUNKER).ready) < 2:
+        #     if self.can_afford(UnitTypeId.BUNKER):
+        #         workers: Units = self.workers.gathering
+        #         if (
+        #             workers and self.townhalls
+        #         ):
+        #             worker: Unit = workers.furthest_to(workers.center)
 
-                    bunker_placement_positions: Set[Point2] = self.main_base_ramp.corner_depots
+        #             bunker_placement_positions: Set[Point2] = self.main_base_ramp.corner_depots
                     
-                    if len(bunker_placement_positions) > 0:                    
-                        bunkers: Units = self.structures.of_type({UnitTypeId.BUNKER})
-                        if bunkers:
-                            bunker_placement_positions: Set[Point2] = {
-                                d
-                                for d in bunker_placement_positions if bunkers.closest_distance_to(d) > 1
-                            }
-                        if len(bunker_placement_positions) > 0:
-                            target_bunker_location: Point2 = bunker_placement_positions.pop()
-                            workers: Units = self.workers.gathering
-                            if workers:  # if workers were found
-                                worker: Unit = workers.random
-                                self.do(worker.build(UnitTypeId.BUNKER, target_bunker_location))
-        else:
-            for bunker in self.structures(UnitTypeId.BUNKER):
-                if bunker.cargo_left > 0:
-                    marines: Units = self.units(UnitTypeId.MARINE).idle
-                    cnt = 0
-                    
-                    for marine in marines:
-                        if cnt < bunker.cargo_left:
-                            marine.move(bunker.position) # TODO: Aqui, em vez de mover pra posição, ele deveria entrar no bunker
-            
+        #             if len(bunker_placement_positions) > 0:                    
+        #                 bunkers: Units = self.structures.of_type({UnitTypeId.BUNKER})
+        #                 if bunkers:
+        #                     bunker_placement_positions: Set[Point2] = {
+        #                         d
+        #                         for d in bunker_placement_positions if bunkers.closest_distance_to(d) > 1
+        #                     }
+        #                 if len(bunker_placement_positions) > 0:
+        #                     target_bunker_location: Point2 = bunker_placement_positions.pop()
+        #                     workers: Units = self.workers.gathering
+        #                     if workers:  # if workers were found
+        #                         worker: Unit = workers.random
+        #                         self.do(worker.build(UnitTypeId.BUNKER, target_bunker_location))
+                            
                 
             
         #treinando marines
         for rax in self.structures(UnitTypeId.BARRACKS).ready:
-            if self.can_afford(UnitTypeId.MARINE) and len(rax.orders) < 2: # 2 ao mesmo tempo, por conta dos reatores
+            if rax.has_techlab:
+                if self.can_afford(UnitTypeId.MARAUDER):
+                    rax.train(UnitTypeId.MARAUDER)
+            elif self.can_afford(UnitTypeId.MARINE) and len(rax.orders) < 2: # 2 ao mesmo tempo, por conta dos reatores
                 rax.train(UnitTypeId.MARINE)
         
-        marines: Units = self.units(UnitTypeId.MARINE).idle
+        marines: Units = self.units(UnitTypeId.MARINE).ready.sorted_by_distance_to(self.start_location) # Deixe os mais perto da base dependendo ela
+        
         #Iremos sempre manter uma quantidade de defensores na base defendersNumber
         #Alem desses defensores, caso a quantidade restante seja maior que a quantidade minima de atacantes (attackersNumber) mandamos eles para o ataque
         cnt = 0 
         attack = True if marines.amount - defendersNumber > attackersNumber else False 
         for marine in marines:
             if cnt < defendersNumber:
-                marine.attack(targetDefender)
+                sendedMarine = False
+                if len(self.structures(UnitTypeId.BUNKER).ready) > 0: # Se tivermos bunkers
+                    for bunker in self.structures(UnitTypeId.BUNKER): 
+                        if bunker.cargo_left > 0: # E tiver bunker com vaga
+                            self.do(bunker(AbilityId.LOAD_BUNKER, marine)) # Mande um Marine pro bunker
+                            sendedMarine = True
+                if not sendedMarine: # Caso o marine não tenha sido mandado
+                    for target in self.structures.furthest_n_units(self.start_location, 3):
+                        marine.patrol(target.position)
+                    #marine.patrol(self.start_location) # Defenda a base
             elif attack:                
                 marine.attack(targetEnemy)
             cnt+=1
 
+        if attack: # Se estiver atacando
+            for marauder in self.units(UnitTypeId.MARAUDER): # Todos os Marauders devem atacar
+                marauder.attack(targetEnemy)
 
-        if iteration % 100 == 0:
-            #Esse print a gente pode usar pra printar os parâmetros pra entender melhor oq eles retornam e como
-            #as coisas funcionam no geral
-            print("Supply used/left: ", self.supply_used, " ", self.supply_left)
-            print("SELFStructures ", self.structures)
+        # if iteration % 100 == 0:
+        #     #Esse print a gente pode usar pra printar os parâmetros pra entender melhor oq eles retornam e como
+        #     #as coisas funcionam no geral
+        #     print("Supply used/left: ", self.supply_used, " ", self.supply_left)
+        #     print("SELFStructures ", self.structures)
         
 
 
 def main():
     run_game(
         maps.get("AcropolisLE"),
-        [Bot(Race.Terran, unidosDaEc()), Computer(Race.Zerg, Difficulty.Hard)],
+        [Bot(Race.Terran, unidosDaEc()), Computer(Race.Zerg, Difficulty.Harder)],
         realtime=False,
     )
 
